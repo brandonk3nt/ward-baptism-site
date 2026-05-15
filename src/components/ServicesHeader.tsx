8aa5d8e62fc71f1b7c440cb40ae8c7a1c26cbd44
brandonk3nt <brandonk3nt@gmail.com>
2026-05-15 07:53:07 -0700

    updates

--- src/components/ServicesHeader.tsx
+++ src/components/ServicesHeader.tsx
@@ -24,10 +24,9 @@ export default function ServicesHeader({
         height={556}
       />
       <div key="title-credits" className="church-font text-center p-1 py-2">
-        <p className="text-4xl">Rhodes Kent</p>
-        <p className="text-4xl">Baptism</p>
+        <p className="text-2xl">Baptism of</p>
+        <p className="text-4xl py-4">Rhodes Kent</p>
         <p className="text-2xl">May 16, 2026</p>
-        <p className="text-2xl">Dana Ranch Ward</p>
       </div>
 
       <hr className="h-px my-8 bg-gray-400 border-0" />
