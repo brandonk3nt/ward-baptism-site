24616001e1b362c0eb6513557576dc266af65877
brandonk3nt <brandonk3nt@gmail.com>
2026-05-15 04:39:15 -0700

    add img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,46 +7,12 @@ import ServicesHeader from "@/components/ServicesHeader";
 export default function Page() {
   return (
     <div className="w-full h-full">
-      <ServicesHeader heroImage="/img/christ-resurrected.jpg" />
+      <ServicesHeader heroImage="/img/jesus-baptism.jpg" />
       <div id="services" className="mx-2">
         <LineItemLeadership left="Presiding" right="Bishop Shumway" />
         <LineItemLeadership left="Conducting" right="Bishop Shumway" />
         <LineItemLeadership left="Organist" right="Melody Williams" />
         <LineItemLeadership left="Chorister" right="Elizabeth Kasl" />
-
-        <div className="p-8 my-4 rounded bg-sky-100">
-          <div className="text-center">
-            <em className="font-semibold text-lg">Announcements</em>
-          </div>
-          <ol className="list-decimal ml-5 sticky">
-            <li className="pt-1">
-              The Dana Ranch Ward is responsible for cleaning the building
-              beginning March 7th through the end of May. The cleaning schedule
-              is included in the weekly e-mail.
-            </li>
-            <li className="pt-1">
-              Missionary Meal Signup. (
-              <a
-                href="https://www.signupgenius.com/go/10C0F4FADAE2DA7F5CE9-57457318-missionary#/"
-                className="text-sky-600 hover:text-sky-800 underline break-words"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Sign up here
-              </a>
-              )
-            </li>
-            <li className="pt-1">
-              Bishop&apos;s Hang Out: Cereal Sunday. All priests and young women
-              class 16-18 are invited to the Shumway&apos;s home on the third
-              Sunday of each month from 7:00-8:30 pm.
-            </li>
-            <li className="pt-1">
-              Mission and Life Skills Prep for all Juniors and Seniors is held
-              every 2nd and 4th Sunday at 3pm in the Relief Society room.
-            </li>
-          </ol>
-        </div>
 
         <LineItem
           left="Opening Hymn"
