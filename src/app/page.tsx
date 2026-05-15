5aea9dc3c445a875b15f4606c7a3f508a5dbd5ab
brandonk3nt <brandonk3nt@gmail.com>
2026-05-15 07:35:09 -0700

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import LineItem from "@/components/LineItem";
-import LineItemLeadership from "@/components/LineItemLeadership";
 import LineItemBold from "@/components/LineItemBold";
 import ServicesFooter from "@/components/ServicesFooter";
 import ServicesHeader from "@/components/ServicesHeader";
@@ -9,58 +8,19 @@ export default function Page() {
     <div className="w-full h-full">
       <ServicesHeader heroImage="/img/jesus-baptism.jpg" />
       <div id="services" className="mx-2">
-        <LineItemLeadership left="Presiding" right="Bishop Shumway" />
-        <LineItemLeadership left="Conducting" right="Bishop Shumway" />
-        <LineItemLeadership left="Organist" right="Melody Williams" />
-        <LineItemLeadership left="Chorister" right="Elizabeth Kasl" />
 
-        <LineItem
-          left="Opening Hymn"
-          middle="The Spirit of God"
-          right="2"
-          link="https://www.churchofjesuschrist.org/media/music/songs/the-spirit-of-god?lang=eng"
-        />
-        <LineItem left="Invocation" right="By Invitation" />
+        <LineItem left="Opening Song" right="Baptism" />
+        <LineItem left="Opening Prayer" right="Lux Heywood" />
 
-        <LineItemBold middle="Ward Business" />
+        <LineItemBold middle="Baptism" />
 
-        <LineItem
-          left="Sacrament Hymn"
-          middle="O God, The Eternal Father"
-          right="175"
-          link="https://www.churchofjesuschrist.org/media/music/songs/o-god-the-eternal-father?lang=eng"
-        />
+        <LineItemBold middle="Confirmation" />
 
-        <LineItemBold middle="Administration of the Sacrament" />
+        <LineItem left="Talk on Baptism" right="Chris Cooley" />
+        <LineItem left="Talk on the Holy Ghost" right="Chris Cooley" />
+        <LineItem left="Music Number" right="Rhodes' Brothers" />
 
-        <LineItemBold middle="Bearing of Testimonies" />
-
-        {/*
-        <LineItem
-          left="Special Musical Number"
-          middle="Gethsemane"
-          right="Primary, Ward Choir"
-        />
-
-        <LineItem left="Speaker" right="Chris Cooley" />
-
-        <LineItem
-          left="Special Musical Number"
-          middle="There is a Green Hill Far Away, This is the Christ"
-          right="Ward Choir"
-        />
-
-        <LineItem left="Speaker" right="Shaeli Kersee" />
-        */}
-
-        <LineItem
-          left="Closing Hymn"
-          middle="I Know That My Redeemer Lives"
-          right="136"
-          link="https://www.churchofjesuschrist.org/media/music/songs/i-know-that-my-redeemer-lives?lang=eng"
-        />
-
-        <LineItem left="Benediction" right="By Invitation" />
+        <LineItem left="Closing Prayer" right="Graham Kent" />
       </div>
       <ServicesFooter />
     </div>
--- src/components/ServicesHeader.tsx
+++ src/components/ServicesHeader.tsx
@@ -18,7 +18,7 @@ export default function ServicesHeader({
         height={2560}
       />
       <Image
-        src="/img/modern-logo.png"
+        src="/img/retro-logo.png"
         className="m-auto w-3/4 md:w-1/2 xl:w-1/3"
         alt="church-logo"
         width={944}
@@ -26,8 +26,8 @@ export default function ServicesHeader({
       />
       <div key="title-credits" className="church-font text-center p-1 py-2">
         <p className="text-2xl">Dana Ranch Ward</p>
-        <p className="text-3xl">Sacrament Meeting</p>
-        <p className="text-2xl">April 12, 2026</p>
+        <p className="text-3xl">Baptism Services for Rhodes Kent</p>
+        <p className="text-2xl">May 16, 2026</p>
       </div>
 
       <div className="pt-4">
