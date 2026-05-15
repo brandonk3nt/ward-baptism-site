b7ddec992fe57bb742e195d9b239a1579558a98e
brandonk3nt <brandonk3nt@gmail.com>
2026-05-15 07:38:26 -0700

    ujpdate

--- src/components/ServicesHeader.tsx
+++ src/components/ServicesHeader.tsx
@@ -18,7 +18,7 @@ export default function ServicesHeader({
         height={2560}
       />
       <Image
-        src="/img/retro-logo.png"
+        src="/img/modern-logo.png"
         className="m-auto w-3/4 md:w-1/2 xl:w-1/3"
         alt="church-logo"
         width={944}
@@ -26,7 +26,9 @@ export default function ServicesHeader({
       />
       <div key="title-credits" className="church-font text-center p-1 py-2">
         <p className="text-2xl">Dana Ranch Ward</p>
-        <p className="text-3xl">Baptism Services for Rhodes Kent</p>
+        <p className="text-3xl">Baptism Services</p>
+        <p className="text-3xl">for</p>
+        <p className="text-3xl">Rhodes Kent</p>
         <p className="text-2xl">May 16, 2026</p>
       </div>
 
