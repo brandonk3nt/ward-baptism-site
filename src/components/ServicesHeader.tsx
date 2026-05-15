1da9d1c7964b109c2fa8a8ccdb3bfeea103bf8e7
brandonk3nt <brandonk3nt@gmail.com>
2026-05-15 07:53:56 -0700

    remove logo

--- src/components/ServicesHeader.tsx
+++ src/components/ServicesHeader.tsx
@@ -16,13 +16,13 @@ export default function ServicesHeader({
         width={3840}
         height={2560}
       />
-      <Image
+      {/* <Image
         src="/img/modern-logo.png"
         className="m-auto w-3/4 md:w-1/2 xl:w-1/3"
         alt="church-logo"
         width={944}
         height={556}
-      />
+      /> */}
       <div key="title-credits" className="church-font text-center p-1 py-2">
         <p className="text-2xl">Baptism of</p>
         <p className="text-4xl py-4">Rhodes Kent</p>
