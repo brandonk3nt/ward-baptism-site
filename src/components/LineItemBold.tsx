cc584e0961fd39cf788dea75954b2b0221fd2a3c
brandonk3nt <brandonk3nt@gmail.com>
2026-05-16 12:26:53 -0700

    stone

--- src/components/LineItemBold.tsx
+++ src/components/LineItemBold.tsx
@@ -6,7 +6,7 @@ interface LineItemProps {
 
 const LineItem: FC<LineItemProps> = ({ middle }) => {
   return (
-    <div className="text-center py-8 my-4 rounded bg-olive-200">
+    <div className="text-center py-8 my-4 rounded bg-stone-200">
       <em className="font-semibold text-lg">{middle}</em>
     </div>
   );
