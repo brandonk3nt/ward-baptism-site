c2077ee78ccc74e23e52f9b833b737cd2ab55785
brandonk3nt <brandonk3nt@gmail.com>
2026-05-16 12:12:19 -0700

    upgrade tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
         <LineItemLeadership left="Chorister" right="Valerie Heywood" />
 
         <div className="pt-4">
-          <div className="flex justify-center font-sm truncate zero-auto max-75">***</div>
+          <div className="flex font-thin justify-center font-sm truncate zero-auto max-75">* * *</div>
         </div>
 
         <LineItem left="Talk on Baptism" right="Jaxon Kent" />
